9570132ec3b39c75a7405c61811e58bb096b89b4
levyxx <levy.b.j.s@gmail.com>
2020-01-02 22:28:20 -0500

    Need to fix malformed HTML documents

--- tests/test_htmls.py
+++ tests/test_htmls.py
@@ -11,29 +11,34 @@ def test_simple_pos():
     assert a.is_opinion()
 
 
-# def test_multilevel_pos():
-#     a = _load_html_as_article('newsweek-pos.html')
-#     assert a.is_opinion()
+def test_multilevel_pos():
+    a = _load_html_as_article('https://www.newsweek.com/corporate-social-responsibility-sham-opinion-1480082',
+                              'newsweek-pos.html')
+    assert a.is_opinion()
 
 
-# def test_contains_pos():
-#     a = _load_html_as_article('observer-pos.html')
-#     assert a.is_opinion()
+def test_contains_pos():
+    a = _load_html_as_article('https://observer.com/2019/07/democrat-leadership-fighting-legal-weed-2020-election/',
+                              'observer-pos.html')
+    assert a.is_opinion()
 
 
-# def test_simple_neg():
-#     a = _load_html_as_article('washingtonpost-neg.html')
-#     assert a.is_opinion()
+def test_simple_neg():
+    a = _load_html_as_article('https://www.washingtonpost.com/weather/2020/01/02/amid-bush-fire-crisis-this-weekend-may-bring-australia-its-most-dangerous-fire-weather-conditions-yet/?hpid=hp_hp-more-top-stories_australia-forecast-320pm-1-duplicate%3Ahomepage%2Fstory-ans',
+                              'washingtonpost-neg.html')
+    assert not a.is_opinion()
 
 
-# def test_multilevel_neg():
-#     a = _load_html_as_article('newsweek-neg.html')
-#     assert a.is_opinion()
+def test_multilevel_neg():
+    a = _load_html_as_article('https://www.newsweek.com/taiwan-president-vows-ensure-stability-national-army-after-military-chief-dies-helicopter-1480021',
+                              'newsweek-neg.html')
+    assert not a.is_opinion()
 
 
-# def test_contains_neg():
-#     a = _load_html_as_article('observer-neg.html')
-#     assert a.is_opinion()
+def test_contains_neg():
+    a = _load_html_as_article('https://observer.com/2020/01/warren-buffett-berkshire-hathaway-decline-tiffany-acquisition-before-lvmh-deal/',
+                              'observer-neg.html')
+    assert not a.is_opinion()
 
 
 def _load_html_as_article(url: str, fpath: str) -> str:
